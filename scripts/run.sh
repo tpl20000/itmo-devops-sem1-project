1190a15a4ad0c4d386a7d961aea759c94639506d
Михаил Те <tpl20000@yandex.ru>
2025-01-11 07:38:01 +0700

    Fixed run.sh nohup issue

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,5 @@ echo "Building application..."
 go build -o sem1-proj .
 
 echo "Running program..."
-nohup ./sem1-proj > output.log
+nohup ./sem1-proj > output.log 2>&1 &
+echo "Program is running. PID: $!"
